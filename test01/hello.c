45e6830c28253f1582e6d299e09c220109c80eb5
210854103-lvkaige <2480762082@qq.com>
2023-10-08 16:44:55 +0800

    完成了前三个练习

---
+++ test01/hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	puts("hello world!");
+	//puts("hell world!");
+	//puts("hello word!");
+	//puts("helo world! ");
+	//puts("hello wood!");
+	for(int i=1;i<=10;i++){
+		printf("%d. hello world!\n",i);
+	}
+	return 0;
+}
