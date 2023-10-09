41c7750e9ee25b6746c30211ea6729504ab6d2aa
210854103-lvkaige <2480762082@qq.com>
2023-10-09 12:47:06 +0800

    完成了结构体练习

---
+++ test16/test.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct person{
+	char *name;
+	int age;
+};
+
+struct person *create_person(char *n,int num){
+	struct person *who=(struct person*)malloc(sizeof(struct person));
+	who->name=n;
+	who->age=num;
+	return who;
+};
+
+void delete(struct person* who){
+	free(who);
+}
+
+int main(){
+	struct person *tom=create_person("tom",30);
+	printf("%s\n %d\n",tom->name,tom->age);
+	delete(tom);
+	return 0;
+}
